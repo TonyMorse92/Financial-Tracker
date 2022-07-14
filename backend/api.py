68d3824350a2fe9384570419d8300f64191eaca1
TonyMorse92 <anthonymorse92@gmail.com>
2022-07-14 17:23:12 -0400

    Added to flask so route would update on refresh for conneciton testing.

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,8 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route("/test")
 def test_route():
-    return {"message": "Testing the connection."}
+    return {"message": f"Testing the connection. I'm thinking of a number {random.random()}"}
